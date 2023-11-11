726fcb57e979797fd01b6bd6d1cc5f2c296520d2
Yurii Rybachok <drummer2009@ukr.net>
2023-11-11 11:15:11 +0200

    setup server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const stytch = require('stytch');
+const app = express();
+require('dotenv').config();
+
+const client = new stytch.Client({
+  project_id: process.env.STYTCH_PID,
+  secret: process.env.STYTCH_SECRET,
+  env: process.env.STYTCH_ENV === 'test' ? stytch.envs.test : stytch.envs.live,
+});
+
+app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
