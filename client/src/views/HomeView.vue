b57a111093558211f3bb76fe4ec185a6c46de620
Yurii Rybachok <drummer2009@ukr.net>
2023-11-11 16:57:59 +0200

    create home page with logout

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const Logout = () => {
+    localStorage.removeItem("session_token")
+    router.push('/login')
+}
+</script>
+
 <template>
-    <h1>Home</h1>
+    <main>
+        <section class="container mx-auto p-4">
+            <h1 class="text-4xl mb-8 text-center">Home</h1>
+            <p class="text-gray-500 text-center">
+                Welcome to the home page, <span class="link" @click="Logout">Logout?</span>
+            </p>
+        </section>
+    </main>
 </template>
