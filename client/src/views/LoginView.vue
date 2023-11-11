3089e2937eb34ae4b52fbbb0a1bf14e65d06a44c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-11 16:23:58 +0200

    create login page & login user with google

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { loadStytch } from '@stytch/stytch-js';
+import { onMounted } from 'vue';
+
+const setup = async () => {
+  const Stytch = await loadStytch();
+
+  let stytch = Stytch('public-token-test-c0408a84-4270-4bcb-9f9c-086384eff4f5').mount({
+    elementId: '#gtap',
+    loginOrSignupView: {
+      products: ['oauth'],
+      oauthOptions: {
+        providers: [{ type: 'google', one_tap: true, position: 'embedded' }],
+      },
+    },
+    style: {
+        width: 'calc(100% - 20px)',
+    }
+  });
+};
+
+onMounted(() => {
+    setup()
+})
+</script>
+
 <template>
-    <h1>Login</h1>
+  <main class="login min-h-screen bg-gradient-to-br from-green-500 to-cyan-400">
+    <section class="container mx-auto pt-16 pb-4 px-4">
+      <h1 class="text-4xl mb-8 text-center font-black uppercase">G-Tap Login</h1>
+
+      <div class="bg-gray-700 max-w-md mx-auto rounded-lg p-4">
+        <div id="gtap"></div>
+      </div>
+    </section>
+  </main>
 </template>
--- server/server.js
+++ server/server.js
@@ -9,50 +9,50 @@ const client = new stytch.Client({
   env: process.env.STYTCH_ENV === 'test' ? stytch.envs.test : stytch.envs.live,
 });
 
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/auth', async (req, res) => {
-    const { token } = req.query
-    try {
-        const response = await client.oauth.authenticate(token, {
-            session_management_type: 'stytch',
-            session_duration_minutes: 5
-        })
+  const { token } = req.query;
+  try {
+    const response = await client.oauth.authenticate(token, {
+      session_management_type: 'stytch',
+      session_duration_minutes: 5,
+    });
 
-        const session_token = response.session.stytch_session.session_token
+    const session_token = response.session.stytch_session.session_token;
 
-        res.redirect(`http://localhost:3000?session_token=${session_token}`);
-    } catch (err) {
-        rea.status(400).json({
-            success: false,
-            message: "Error authenticating",
-            error: err
-        })
-    }
-})
+    res.redirect(`http://localhost:3000?session_token=${session_token}`);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'Error authenticating',
+      error: err,
+    });
+  }
+});
 
 app.post('/verify', async (req, res) => {
-    const { token } = req.body
-    try {
-        await client.sessions.authenticate({session_token: token})
-        res.status(200).json({
-            success: true,
-            message: "Session Verified"
-        })
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            message: "Error Authenticating",
-            error: err
-        })
-    }
-})
+  const { token } = req.body;
+  try {
+    await client.sessions.authenticate({ session_token: token });
+    res.status(200).json({
+      success: true,
+      message: 'Session Verified',
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'Error Authenticating',
+      error: err,
+    });
+  }
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+app.listen(port, () => console.log(`Server listening on port ${port}`));
