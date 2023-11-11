aeb2b194fcfd44d43e4846e1da03e58ad7ec2ffc
Yurii Rybachok <drummer2009@ukr.net>
2023-11-11 11:48:12 +0200

    setup client

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import VueToast from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
+import './main.css'
+import router from './router'
 
-createApp(App).mount('#app')
+const vm = createApp(App).use(router).use(VueToast).mount('#app')
+
+export default vm
